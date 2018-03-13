0a36878f376f93c6ca5a80378411c3adf25f61fc
Kassymov Shyngys <chika.kasymov@gmail.com>
2018-03-13 13:52:27 +0600

    Added option to choose files from gallery for new tasks and requests

--- Life/Controllers/TasksAndRequests/RequestFormViewController.swift
+++ Life/Controllers/TasksAndRequests/RequestFormViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import FileProvider
+import IQMediaPickerController
+import Photos
 import RxSwift
 import RxCocoa
 import SnapKit
@@ -38,7 +40,7 @@ class RequestFormViewController: UIViewController, Stepper {
 
     // MARK: - Methods
 
-    private func pickAttachments() {
+    private func pickFromDocuments() {
         let fileExplorer = FileExplorerViewController()
         fileExplorer.canChooseFiles = true
         fileExplorer.allowsMultipleSelection = true
@@ -49,6 +51,76 @@ class RequestFormViewController: UIViewController, Stepper {
             Filter.extension("jpeg")
         ]
         self.present(fileExplorer, animated: true, completion: nil)
+    }
+
+    private func pickFromGallery() {
+        let vc = IQMediaPickerController()
+        vc.allowsPickingMultipleItems = true
+        vc.delegate = self
+        vc.mediaTypes = [
+            NSNumber(value: IQMediaPickerControllerMediaType.photo.rawValue),
+            NSNumber(value: IQMediaPickerControllerMediaType.video.rawValue)
+        ]
+        vc.sourceType = .library
+        present(vc, animated: true, completion: nil)
+    }
+
+    private func pickAttachments() {
+        let alert = UIAlertController(
+            title: NSLocalizedString("choose_option", comment: ""),
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        alert.popoverPresentationController?.sourceView = view
+
+        let captureAction = UIAlertAction(
+            title: NSLocalizedString("pick_from_documents", comment: ""),
+            style: .default) { [weak self] _ in
+                self?.pickFromDocuments()
+        }
+        alert.addAction(captureAction)
+        let libraryAction = UIAlertAction(
+            title: NSLocalizedString("pick_from_gallery", comment: ""),
+            style: .default) { [weak self] _ in
+                self?.pickFromGallery()
+        }
+        alert.addAction(libraryAction)
+        let cancelAction = UIAlertAction(
+            title: NSLocalizedString("cancel", comment: ""),
+            style: .cancel,
+            handler: nil)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true, completion: nil)
+    }
+
+    private func add(videlURL: URL) {
+        let fetchResult = PHAsset.fetchAssets(withALAssetURLs: [videlURL], options: nil)
+        if let phAsset = fetchResult.firstObject {
+            PHImageManager.default().requestAVAsset(
+                forVideo: phAsset,
+                options: PHVideoRequestOptions(),
+                resultHandler: { (asset, _, _) -> Void in
+                    if let asset = asset as? AVURLAsset {
+                        let videoData = try? Data(contentsOf: asset.url)
+
+                        let tempDir = NSTemporaryDirectory()
+                        let tempDirPath = URL(fileURLWithPath: tempDir)
+                        let videoName = UUID().uuidString
+                        let videoPath = tempDirPath.appendingPathComponent("\(videoName).MOV")
+                        if FileManager.default.fileExists(atPath: videoPath.path) {
+                            try? FileManager.default.removeItem(at: videoPath)
+                        }
+
+                        let writeResult = try? videoData?.write(to: videoPath)
+                        if writeResult != nil {
+                            var attachments = (try? self.viewModel.attachments.value()) ?? []
+                            attachments.append(videoPath)
+                            self.viewModel.attachments.onNext(attachments)
+                        }
+                    }
+            })
+        }
     }
 
     // MARK: - UI
@@ -145,6 +217,47 @@ class RequestFormViewController: UIViewController, Stepper {
         }).disposed(by: disposeBag)
     }
 
+}
+
+extension RequestFormViewController: IQMediaPickerControllerDelegate, UINavigationControllerDelegate {
+    func mediaPickerController(_ controller: IQMediaPickerController,
+                               didFinishMediaWithInfo info: [AnyHashable : Any]) {
+        viewModel.attachments.onNext([])
+
+        for key in info.keys where key is String {
+            if let dicts = info[key] as? [[String: Any]] {
+                for dict in dicts {
+                    if let image = dict[IQMediaImage] as? UIImage {
+                        let tempDir = NSTemporaryDirectory()
+                        let tempDirPath = URL(fileURLWithPath: tempDir)
+                        let imageName = UUID().uuidString
+                        let imagePath = tempDirPath.appendingPathComponent("\(imageName).jpg")
+                        if FileManager.default.fileExists(atPath: imagePath.path) {
+                            try? FileManager.default.removeItem(at: imagePath)
+                        }
+                        let imageData = UIImageJPEGRepresentation(image, 1.0)
+                        do {
+                            try imageData?.write(to: imagePath)
+
+                            var attachments = (try? self.viewModel.attachments.value()) ?? []
+                            attachments.append(imagePath)
+                            self.viewModel.attachments.onNext(attachments)
+                        } catch {
+                            print("Failed to write image at path \(imagePath)")
+                        }
+                    } else if let url = dict[IQMediaURL] as? URL {
+                        add(videlURL: url)
+                    } else if let url = dict[IQMediaAssetURL] as? URL {
+                        add(videlURL: url)
+                    }
+                }
+            }
+        }
+    }
+
+    func mediaPickerControllerDidCancel(_ controller: IQMediaPickerController) {
+        print("Media pick cancelled ...")
+    }
 }
 
 extension RequestFormViewController: FileExplorerViewControllerDelegate {
